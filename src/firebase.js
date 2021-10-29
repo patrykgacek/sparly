bfb0a08eb2201ffb8006716af8ef098c0f04ace1
Gacus <haniv1001@gmail.com>
2021-10-29 23:57:54 +0200

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZAo9KBwRiEPIC038UZ8ly-UL4y9HguR0",
+  authDomain: "formal-vortex-225715.firebaseapp.com",
+  databaseURL: "https://formal-vortex-225715-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "formal-vortex-225715",
+  storageBucket: "formal-vortex-225715.appspot.com",
+  messagingSenderId: "348447624562",
+  appId: "1:348447624562:web:635853a0c54bf8d43e61eb"
+}
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig)
+
+export default firebase
