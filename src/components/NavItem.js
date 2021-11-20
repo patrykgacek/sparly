35cc0ad6c55dae28a2e6490ad49ac489a09b7eac
Gacus <haniv1001@gmail.com>
2021-11-20 02:00:54 +0100

    update layout style

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -6,7 +6,7 @@ const NavItem = ({icon, to, children}) => {
             <NavLink 
             className="text-xl block hover:bg-gray-200 rounded-md py-2 px-3 mb-3"
             to={to}>
-                <div className="inline-block w-5 mr-2">
+                <div className="inline-block w-5 mr-2 text-gray-800">
                     <FontAwesomeIcon icon={icon} />
                 </div>
                 { children }
