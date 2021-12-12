739e56d72d735f386a63992766128bc71aa0e1a4
Kajo337 <56159605+Kajo337@users.noreply.github.com>
2021-12-12 15:53:25 +0100

    test1 z Patrykiem

--- src/pages/Savings.js
+++ src/pages/Savings.js
@@ -3,7 +3,10 @@ import Layout from "../components/Layout";
 const Savings = () => {
     return (
         <Layout>
+            <main>
             <div>Savings</div>
+            <div></div>
+            </main>
         </Layout>
     )
 }
