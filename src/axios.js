cff07d40375f6d28840785343d810a26d057732e
Gacus <haniv1001@gmail.com>
2021-12-23 17:38:04 +0100

    delete axios bigos is useless

--- src/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    baseURL = 'https://formal-vortex-225715-default-rtdb.europe-west1.firebasedatabase.app/'
-})
-
-export default instance
