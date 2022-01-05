97e20a5bb01e9fe530e30750a204f37eaa0efbdb
Mateusz Machura <machura.mateusz00@gmail.com>
2022-01-06 00:34:56 +0100

    statistics

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -32,16 +32,26 @@ const Statistics = () => {
     var expenseTemp = []
     var incomeTemp = []
     var categories = []
+    function formatDate(d){
+        let year = d.getFullYear();
+        let month = d.getMonth()+1
+        let day = d.getDate();
+        if(month<10){
+            month = `0${month}`
+        }
+        if(day<10){
+            day = `0${day}`
+        }
+        return `${year}-${month}-${day}`
+    }
     const [dateFrom, setFromDate] = useState(()=>{
         let d = new Date();
-        d.setDate(d.getDate() - 30); 
-        return d.getMonth()<10 ? `${d.getFullYear()}-0${d.getMonth()+1}-${d.getDate()}`:
-         `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`
+        d.setDate(d.getDate() - 30);
+        return formatDate(d)
     });
     const [dateTo, setToDate] = useState(()=>{
         let d = new Date();
-        return d.getMonth()<10 ? `${d.getFullYear()}-0${d.getMonth()+1}-${d.getDate()}`:
-         `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}`
+        return formatDate(d)
     });
     const { expense,income, setLoadExpense, setLoadIncome } = useDatabase()
     useEffect(() => {
@@ -91,31 +101,196 @@ const Statistics = () => {
     const handleDateTo = e => {
         setToDate(e.target.value)
     }
- 
+    function isValidDate(d) {
+        var timestamp = Date.parse(d);
+        console.log(timestamp)
+        if (isNaN(timestamp) == false) {
+            
+            return true
+        }
+        return false;
+    }
+    /**
+     * Compeare two dates
+     * @param {*} data1 format required dd.mm.yyyy
+     * @param {*} data2 format required dd.mm.yyyy
+     * @returns true if date1 is newer than date2
+     */
+    function dateNewerThan(data1, data2){
+        //var d1 = data1.split(/.-/).map(Number);
+        //var d2 = data2.split(/.-/).map(Number);
+        var d1 = data1.split('-').map(Number);
+        var d2 = data2.split('-').map(Number);
+        if(d1[0]>d2[0]){
+            return true;
+        }
+        else if(d1[0] == d2[0]){
+            if(d1[1]>d2[1]){
+                return true;
+            }
+            else if(d1[1] == d2[1]){
+                if(d1[2]>d2[2]){
+                    return true;
+                }
+            }
+        }
+        return false
+    }
+    function dateNewerOrEqualThan(data1, data2){
+        //var d1 = data1.split(/.-/).map(Number);
+        //var d2 = data2.split(/.-/).map(Number);
+        var d1 = data1.split('-').map(Number);
+        var d2 = data2.split('-').map(Number);
+        if(d1[0]>d2[0]){
+            return true;
+        }
+        else if(d1[0] == d2[0]){
+            if(d1[1]>d2[1]){
+                return true;
+            }
+            else if(d1[1] == d2[1]){
+                if(d1[2]>=d2[2]){
+                    return true;
+                }
+            }
+        }
+        return false
+    }
+   
+    function getDateRange(data){
+        var dataNew = []
+        for(var i =0;i<data.length;i++){
+            if(dateNewerOrEqualThan(data[i][1], dateFrom) && dateNewerOrEqualThan(dateTo, data[i][1])){
+                dataNew.push(data[i])
+            }
+        }
+    
+        return dataNew
+    }
+    /**
+     * Sort by older date
+     * @param {*} dateIndex idex od date argument in array of arrays
+     * @param {*} data array of arrays
+     */
+    function sortByDate(data){
+        var i, j; 
+        var n = data.length
+        for (i = 0; i < n-1; i++){
+            for (j = 0; j < n-i-1; j++) {
+                if (dateNewerThan(data[j][1], data[j+1][1])) {
+                    let temp = data[j]
+                    data[j] = data[j+1]
+                    data[j+1] = temp
+                }
+            }
+        }
+       
+    }
+    
+    function groupByMonthAverage(data){
+        var group = []
+        for(let m = 1;m<=12;m++){
+            let month = {month: m, sum:0, records:0, average:0.0}
+            group.push(month)
+        }
+        Array.prototype.forEach.call(data, function(record){
+            let month = parseInt(record[1].split('.')[1])-1
+            group[month].sum += parseInt(record[2])
+            group[month].records += 1
+        })
+        for(let m = 0;m<12;m++){
+            group[m].average = group[m].sum/group[m].records
+        }
+        return group
+    }
+    function getMonth(data, monthNumber){
+        var dataNew = []
+        for(var i =0;i<data.length;i++){
+            if(parseInt(data[i][1].split('.')[1])==monthNumber){
+                dataNew.push(data[i])
+            }
+        }
+        return dataNew
+    }
+   function devConvertDate(date){
+       let t = date.split('.')
+       return `${t[2]}-${t[1]}-${t[0]}`
+   }
+   function normalizeData(){
+    sortByDate(expenseTemp)
+    sortByDate(incomeTemp)
+       if(incomeTemp.length!=expenseTemp.length){
+            if(incomeTemp.length<expenseTemp.length){
+                let incomeNew = []
+                for(var i=0;i<expenseTemp.length;i++){
+                    if(incomeTemp.length<=i){
+                        incomeNew.push([expenseTemp[i][0],expenseTemp[i][1],0,0,0])
+                        continue
+                    }
+                    if(incomeTemp.length<=i+1&& incomeTemp[i][1]==expenseTemp[i][1]){
+                        incomeNew.push(incomeTemp[i])
+                    }
+                    else{
+                        incomeNew.push([expenseTemp[i][0],expenseTemp[i][1],0,0,0])
+                    }
+                }
+                incomeTemp = incomeNew
+            }
+            else{
+                let expenseNew = []
+                for(var i=0;i<incomeTemp.length;i++){
+                    if(expenseTemp.length<=i){
+                        expenseNew.push([incomeTemp[i][0],incomeTemp[i][1],0,0,0])
+                        continue
+                    }
+                    
+                    if(expenseTemp[i][1]==expenseTemp[i][1]){
+                        expenseNew.push(expenseTemp[i])
+                    }
+                    else{
+                        expenseNew.push([incomeTemp[i][0],incomeTemp[i][1],0,0,0])
+                    }
+                    console.log(expenseTemp[i], incomeTemp[i])
+                }
+                expenseTemp = expenseNew
+            }
+       }
+   }
     const drawCharts = () => {
         console.log(dateFrom, dateTo)
         expenseTemp.length = 0;
         incomeTemp.length = 0;
-        console.log(expense)
-        
+
         Object.keys(expense).map(key => { 
            // console.log(expense[key][EXPENSE.NAME])
            // console.log(expense[key][EXPENSE.PRICE]) 
             expenseTemp.push([expense[key][EXPENSE.NAME],
-                new Date(expense[key][EXPENSE.DATE]),
+                devConvertDate(expense[key][EXPENSE.DATE]),
                 expense[key][EXPENSE.PRICE],
                 expense[key][EXPENSE.FAMILY_MEMBER],
                 expense[key][EXPENSE.CATEGORY]])
         })
         Object.keys(income).map(key => { 
              incomeTemp.push([income[key][INCOME.NAME],
-                income[key][INCOME.DATE],
+                devConvertDate(income[key][INCOME.DATE]),
                 income[key][INCOME.PRICE],
                 income[key][INCOME.FAMILY_MEMBER],
                 income[key][INCOME.CATEGORY]])
          })
-        var aa = expenseTemp.sort((a, b) => b[1] - a[1])
-         console.log(aa)
+        expenseTemp =  getDateRange(expenseTemp);
+        incomeTemp =  getDateRange(incomeTemp)
+
+        normalizeData()
+       // var g = groupByMonthAverage(expenseTemp)
+       //  console.log(g)
+        //expenseTemp =  getMonth(expenseTemp, 1);
+        //incomeTemp =  getMonth(incomeTemp, 1)
+        
+       
+       // incomeTemp.pop()
+       // incomeTemp.pop()
+       // incomeTemp.pop()
+        console.log(expenseTemp, incomeTemp)
         setData(setChart())
         setData1(setChart1())
     }
