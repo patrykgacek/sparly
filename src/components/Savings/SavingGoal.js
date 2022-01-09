97eaa4d14462eedf862696d78670caff336dd91d
Kajo337 <56159605+Kajo337@users.noreply.github.com>
2022-01-09 13:30:51 +0100

    saveingsDataBase

--- src/components/Savings/SavingGoal.js
+++ src/components/Savings/SavingGoal.js
@@ -1,6 +1,7 @@
 import { useState } from "react"
-import { SAVINGS_GOAL } from "../../constans"
+import { SAVINGS_GOAL, USER_INFO } from "../../constans"
 import { useDatabase } from "../../context/DatabaseContext"
+
 
 const SavingGoal = props => {
 
@@ -15,6 +16,8 @@ const SavingGoal = props => {
     const [completionDate, setcompletionDate] = useState(props.completionDate) 
     const [timeInterval, settimeInterval] = useState(props.timeIn) 
     const [description, setdescription] = useState(props.description) 
+    const [deposit, setdeposit] = useState("") 
+    const [withdraw, setwithdraw] = useState("") 
     
 
     const handleName = e => setName(e.target.value)
@@ -22,8 +25,10 @@ const SavingGoal = props => {
     const handlecompletionDate = e => setcompletionDate(e.target.value)
     const handletimeInterval = e => settimeInterval(e.target.value)
     const handledescription = e => setdescription(e.target.value)
+    const handledeposit = e => setdeposit(e.target.value)
+    const handlewithdraw = e => setwithdraw(e.target.value)
     
-    const {updateSavingsGoal} = useDatabase() // Baza danych
+    const {updateSavingsGoal,userInfo,updateUserInfo} = useDatabase() // Baza danych
     
 
 
@@ -68,38 +73,57 @@ const SavingGoal = props => {
             
         };
         updateSavingsGoal(props.id,newSavingGoal);
-        setisEditForm(false);
+        setisEditForm(true);
     }
 
     const handleDelete = () =>{
         updateSavingsGoal(props.id,null);
     }
 
-    return (
-        <div className={`border-solid border-2 border-black-600 py-auto text-3xl rounded-md ${isCancelForm ? "h-52" : "h-80"} ${isPayToGoalForm ? "h-52" : "h-80"} ${isPayFromGoalForm ? "h-52" : "h-80"} ${isEditForm ? "h-52" : "h-80"}`}>
-                    <div className="w-3/12 h-52 float-left text-center border-solid border-2 border-black-600">
+    const payToGoal = e =>{
+        e.preventDefault();
+        const newActualAmount = parseFloat(props.actualA) + parseFloat(deposit);
+        const newBalance = parseFloat(userInfo[USER_INFO.BALANCE]) - parseFloat(deposit);
+        updateSavingsGoal(props.id+"/"+[SAVINGS_GOAL.ACTUAL_AMOUNT],newActualAmount);
+        updateUserInfo([USER_INFO.BALANCE],newBalance);
+        setisPayToGoalForm(true);
+        setdeposit('');
+    }
 
-                        <label for="exampleFormControlInput1" className="form-label text-xl w-full inline-block mt-12  text-gray-700">{props.title}</label>
-                        <label for="exampleFormControlInput1" className="form-label text-xl w-full inline-block mt-7  text-gray-700">{props.actualD}</label>
-                        <label for="exampleFormControlInput1" className="form-label text-xl w-full inline-block mt-7  text-gray-700">{props.completionDate}</label>
+    const payFromGoal = e =>{
+        e.preventDefault();
+        const newActualAmount = parseFloat(props.actualA) - parseFloat(withdraw);
+        const newBalance = parseFloat(userInfo[USER_INFO.BALANCE]) + parseFloat(withdraw);
+        updateSavingsGoal(props.id+"/"+[SAVINGS_GOAL.ACTUAL_AMOUNT],newActualAmount);
+        updateUserInfo([USER_INFO.BALANCE],newBalance);
+        setisPayFromGoalForm(true);
+        setwithdraw('');
+    }
+
+    return (
+        <div className={`border-solid py-1 px-1 border-2  border-black-400 text-3xl rounded-lg ${isCancelForm ? "h-52" : "h-80"} ${isPayToGoalForm ? "h-52" : "h-80"} ${isPayFromGoalForm ? "h-52" : "h-80"} ${isEditForm ? "h-52" : "h-80"}`}>
+                    <div className="w-3/12 h-48 inline-block bg-blue-200 float-left text-center border-solid border-2 border-white rounded-lg">
+                        <label for="exampleFormControlInput1" className="form-label text-xl w-full inline-block mt-5  text-gray-700">Goal name: {props.title}</label>
+                        <label for="exampleFormControlInput1" className="form-label text-xl w-full inline-block mt-5  text-gray-700">Create date: {props.actualD}</label>
+                        <label for="exampleFormControlInput1" className="form-label text-xl w-full inline-block mt-5  text-gray-700">Completion date: {props.completionDate}</label>
                     </div>
-                    <div className="w-7/12 h-52 float-left border-solid border-2 border-black-600">
+                    <div className="w-7/12 h-48 float-left bg-blue-200 border-solid border-2 border-white rounded-lg">
                         <div className="w-11/12 bg-gray-200 h-1 mx-auto">
                             <div className="bg-blue-600 h-1 mt-9" style={{width:(props.actualA*100/props.goalAmount)+'%'}}></div>
-                            <label for="exampleFormControlInput1" className="form-label text-xl w-1/3 inline-block mt-7 mb-1 text-gray-700">{props.actualA}</label>
+                            <label for="exampleFormControlInput1" className="form-label text-xl w-1/3 inline-block mt-7 mb-1 text-gray-700"> Actual amount: {props.actualA}</label>
                             <label for="exampleFormControlInput1" className="form-label text-xl w-1/3 inline-block text-center mt-7 mb-1 text-gray-700">{props.actualA*100/props.goalAmount}%</label>
-                            <label for="exampleFormControlInput1" className="form-label text-xl w-1/3 text-right inline-block mt-7 mb-1 text-gray-700">{props.goalAmount}</label>
-                            <label for="exampleFormControlInput1" className="form-label text-xl w-full text-center inline-block mb-1 text-gray-700">{props.timeIn}</label>
-                            <label for="exampleFormControlInput1" className="form-label text-xl w-full text-center inline-block mt-1 mb-1 text-gray-700">{props.desc}</label>
+                            <label for="exampleFormControlInput1" className="form-label text-xl w-1/3 text-right inline-block mt-7 mb-1 text-gray-700">Goal amount: {props.goalAmount}</label>
+                            <label for="exampleFormControlInput1" className="form-label text-xl w-full text-center inline-block mb-1 text-gray-700">Time interwal: {props.timeIn}</label>
+                            <label for="exampleFormControlInput1" className="form-label text-xl w-full text-center inline-block mt-1 mb-1 text-gray-700">Description: {props.description}</label>
                         </div>
 
                     </div>
-                    <div className="w-2/12 h-52 static float-left border-solid border-2 border-black-600">
+                    <div className="w-2/12 h-48 static bg-blue-200 float-left border-solid border-2 border-white rounded-lg">
 
-                        <button className="w-44 h-8 border-solid border-2 rounded-md text-lg ml-1" onClick={zmien3}>Delete goal</button>
-                        <button className="w-44 h-8 border-solid border-2 rounded-md text-lg ml-1" onClick={zmien4}>Deposit to goal</button>
-                        <button className="w-44 h-8 border-solid border-2 rounded-md text-lg ml-1" onClick={zmien5}>Withdraw from goal</button>
-                        <button className="w-44 h-8 border-solid border-2 rounded-md text-lg ml-1" onClick={zmien6}>Update goal</button>
+                        <button className="w-44 h-8 border-solid border-2 bg-white rounded-md text-lg ml-1 mt-5" onClick={zmien3}>Delete goal</button>
+                        <button className="w-44 h-8 border-solid border-2 bg-white rounded-md text-lg ml-1" onClick={zmien4}>Deposit to goal</button>
+                        <button className="w-44 h-8 border-solid border-2 bg-white rounded-md text-lg ml-1" onClick={zmien5}>Withdraw from goal</button>
+                        <button className="w-44 h-8 border-solid border-2 bg-white rounded-md text-lg ml-1" onClick={zmien6}>Update goal</button>
                     </div>
                     {
                         isCancelForm ? ("") : (
@@ -118,20 +142,24 @@ const SavingGoal = props => {
                     {
                         isPayToGoalForm ? ("") : (
 
-                            <div className="flex float-left w-full justify-center">
+                            <form onSubmit={payToGoal} className="flex float-left w-full justify-center">
 
                                 <label for="exampleFormControlInput1" className="form-label mt-8 text-xl inline-block mb-1 text-gray-700"
                                 >How much do you want to deposit:</label
                                 >
+                                
                                 <input
+                                    onChange={handledeposit}
+                                    value={deposit}
                                     type="number"
                                     className="form-control block w-32 h-10 px-3 py-1.5 mt-7 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out mx-2 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     id="exampleNumber0"
                                     placeholder="Amount"
+                                    required
                                 />
                                 <button className="w-44 h-10 border-solid border-2 rounded-md text-lg  hover:bg-black-100 mx-4 mt-7">Submit</button>
 
-                            </div>
+                            </form>
 
 
                         )
@@ -139,20 +167,23 @@ const SavingGoal = props => {
                     {
                         isPayFromGoalForm ? ("") : (
 
-                            <div className="flex float-left w-full justify-center">
+                            <form onSubmit={payFromGoal} className="flex float-left w-full justify-center">
 
                                 <label for="exampleFormControlInput1" className="form-label mt-8 text-xl inline-block mb-1 text-gray-700"
                                 >How much do you want to withdraw from goal:</label
                                 >
                                 <input
+                                    onChange={handlewithdraw}
+                                    value={withdraw}
                                     type="number"
                                     className="form-control block w-32 h-10 px-3 py-1.5 mt-7 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out mx-2 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                                     id="exampleNumber0"
                                     placeholder="Amount"
+                                    required
                                 />
                                 <button className="w-44 h-10 border-solid border-2 rounded-md text-lg  hover:bg-black-100 mx-4 mt-7">Submit</button>
 
-                            </div>
+                            </form>
 
 
                         )
